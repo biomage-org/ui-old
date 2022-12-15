353851dab044297cf483f401130ad07b6772070a
Pol Alvarez <pol.avms@gmail.com>
2022-12-15 19:26:02 +0100

    resuming failed qc pipeline from last successful

--- src/redux/actions/pipeline/runQC.js
+++ src/redux/actions/pipeline/runQC.js
@@ -9,6 +9,8 @@ import {
 import { saveProcessingSettings } from 'redux/actions/experimentSettings';
 import { loadBackendStatus } from 'redux/actions/backendStatus';
 import { loadEmbedding } from 'redux/actions/embedding';
+import { getBackendStatus } from 'redux/selectors';
+import pipelineStatusValues from 'utils/pipelineStatusValues';
 
 const runOnlyConfigureEmbedding = async (experimentId, embeddingMethod, dispatch) => {
   await dispatch(saveProcessingSettings(experimentId, 'configureEmbedding'));
@@ -28,27 +30,52 @@ const runOnlyConfigureEmbedding = async (experimentId, embeddingMethod, dispatch
   );
 };
 
-const runQC = (experimentId) => async (dispatch, getState) => {
-  const { processing } = getState().experimentSettings;
-  const { changedQCFilters } = processing.meta;
+const buildProcessingConfig = (processingConfig, changedQCFilters, qcStatus) => {
+  // if the pipeline has failed and there aren't any changes,
+  // by default start running from the first failed step
+  // const stepsToRun = { ...changedQCFilters };
+  console.log('initial steps to run ', changedQCFilters);
+  console.log('changedQCFilters ', changedQCFilters);
+  console.log('qcStatus ', qcStatus);
 
+  const result = Array.from(changedQCFilters).map((key) => ({
+    name: key,
+    body: { ...processingConfig[key] },
+  }));
+  console.log(qcStatus.status);
+  console.log(qcStatus.status === pipelineStatusValues.FAILED);
+  console.log(changedQCFilters.size);
+  console.log(changedQCFilters.size === 0);
+  console.log(qcStatus.completedSteps.length);
+  console.log(qcStatus.completedSteps.length > 0);
+  if (qcStatus.status === pipelineStatusValues.FAILED
+    && changedQCFilters.size === 0
+    && qcStatus.completedSteps.length > 0) {
+    const lastSuccessfulStep = qcStatus.completedSteps[qcStatus.completedSteps.length - 1];
+    result.push({ name: lastSuccessfulStep, body: { ...processingConfig[lastSuccessfulStep] } });
+    console.log('resulting steps to run ', result);
+  }
+  return result;
+};
+
+const runQC = (experimentId) => async (dispatch, getState) => {
+  const { processing: processingConfig } = getState().experimentSettings;
+  const { pipeline: pipelineStatus } = getBackendStatus(experimentId)(getState()).status;
+  const { changedQCFilters } = processingConfig.meta;
+
+  console.log('pipeline', pipelineStatus);
+  console.log('processing', processingConfig);
   if (changedQCFilters.size === 1 && changedQCFilters.has('configureEmbedding')) {
     runOnlyConfigureEmbedding(
       experimentId,
-      processing.configureEmbedding.embeddingSettings.method,
+      processingConfig.configureEmbedding.embeddingSettings.method,
       dispatch,
     );
 
     return;
   }
 
-  const changesToProcessingConfig = Array.from(changedQCFilters).map((key) => {
-    const currentConfig = processing[key];
-    return {
-      name: key,
-      body: currentConfig,
-    };
-  });
+  const configToRun = buildProcessingConfig(processingConfig, changedQCFilters, pipelineStatus);
 
   try {
     // We are only sending the configuration that we know changed
@@ -65,7 +92,7 @@ const runQC = (experimentId) => async (dispatch, getState) => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          processingConfig: changesToProcessingConfig,
+          processingConfig: configToRun,
         }),
       },
     );
